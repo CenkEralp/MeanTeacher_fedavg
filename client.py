7960f0c18ce6b5eefff8ae85e4088fb46d6dda58
cenk.eralp <cenk.eralp@siemens.com>
2022-07-27 18:14:38 +0300

    a

--- client.py
+++ client.py
@@ -166,7 +166,7 @@ class Client(object):
                 outputs2 = Variable(outputs2.detach().data, requires_grad=False)
                 #outputs2 = Variable(outputs.detach().data, requires_grad=False)
                 
-                semisupervised = False
+                semisupervised = True
                 if semisupervised:
                     loss = loss_f(outputs, labels) + get_current_consistency_weight(e) \
                         * consistency_criterion(outputs2, ema_logit)
